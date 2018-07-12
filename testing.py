f5dac0fb58cf7b14a1302e1248264bd770f54f54
Trevor <rowdeil@gmail.com>
2018-07-12 16:29:25 -0700

    Reddit authentication now works. Requires a configuration file for personal info.

--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
 import requests, os
 import requests.auth
 from fake_useragent import UserAgent
+from pprint import pprint
 
 # enable useragent
 ua = UserAgent()
@@ -14,16 +15,24 @@ confFile = open('conf.txt', 'r')
 lines = confFile.readlines()
 
 # get rid of descriptions and newline characters for data in conf file.
-lines[0] = lines[0][5:-1]
-lines[1] = lines[1][4:-1]
-lines[2] = lines[2][4:-1]
-lines[3] = lines[3][4:0]
+key = lines[0][5:-1]
+appID = lines[1][4:-1]
+un = lines[2][4:-1]
+pw = lines[3][4:]
 
 # Get Access token for reddit api
-client_auth = requests.auth.HTTPBasicAuth(lines[1], lines[0])
-post_data = {"grant_type": "password", "username":lines[3], "password":lines[2]}
+client_auth = requests.auth.HTTPBasicAuth(appID, key)
+post_data = {"grant_type": "password", "username":un, "password":pw}
 headers = {"User-Agent" : ua.ie}
 response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data = post_data, headers = headers)
-print(response.json())
+json = response.json()
+print(json['access_token'])
+
+headers = {"Authorization" : "bearer " + json['access_token'],"User-Agent": ua.ie }
+response = requests.get("https://oauth.reddit.com/api/v1/me", headers = headers)
+
+pprint(response.json())
+
+
 
 
