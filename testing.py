a0eef391660b27c51ef128880351a33254bd5d15
Trevor <rowdeil@gmail.com>
2018-07-12 17:03:27 -0700

    Creating a proper user-agent according to reddit api guidelines.

--- testing.py
+++ testing.py
@@ -3,9 +3,6 @@ import requests.auth
 from fake_useragent import UserAgent
 from pprint import pprint
 
-# enable useragent
-ua = UserAgent()
-
 # change working directory to where program is currently running.
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
@@ -19,20 +16,20 @@ key = lines[0][5:-1]
 appID = lines[1][4:-1]
 un = lines[2][4:-1]
 pw = lines[3][4:]
+ua = "windows:UJjtnyb20g55Cg:v0.0.1 (by /u/zicameau)"
 
 # Get Access token for reddit api
 client_auth = requests.auth.HTTPBasicAuth(appID, key)
 post_data = {"grant_type": "password", "username":un, "password":pw}
-headers = {"User-Agent" : ua.ie}
+headers = {"User-Agent" : ua}
 response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data = post_data, headers = headers)
-json = response.json()
-print(json['access_token'])
+access_resp = response.json()
 
-headers = {"Authorization" : "bearer " + json['access_token'],"User-Agent": ua.ie }
+headers = {"Authorization" : "bearer " + access_resp["access_token"],"User-Agent":ua }
 response = requests.get("https://oauth.reddit.com/api/v1/me", headers = headers)
 
-pprint(response.json())
+##pprint(response.json())
 
-
+pprint(response.headers)
 
 
