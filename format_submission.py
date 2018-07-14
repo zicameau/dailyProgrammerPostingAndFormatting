6c8141eef1a09d13bfce08f209d2f5d6d7c32715
zicameau <rowdeil@gmail.com>
2018-07-13 20:17:35 -0700

    Erased url check as it didn't work with python3. Put a disclaimer at the end of the text that is submitted with the post.

--- format_submission.py
+++ format_submission.py
@@ -14,14 +14,30 @@ while True:
             text = f.read()
             
     except OSError:
+        print(file)
         file = input("Could not open file. Please enter file path again: ")
         continue
     break
 
 ## Add four spaces to all of the from the file.
-text = "    " + text.replace("\n","\n    ")
+text = "   " + text.replace("\n","\n    ")
 
+## Loop until the user enters a proper description.
+while type(description) != str:
+    description = input("You did not enter a description. Please enter one now: ")
+
+## TODO: ensure that the url entered is proper.
+    
+## Add description to the text and put programming version and language at
+## the end. 
 text = description + "\n " + text
 text = "Python Version: " + str(sys.version[:5])+ "\n" + "\n" + text
+
+## Add disclaimer at the end of the comment about this comment.
+disclaimer = " This comment was made with a simple script that I use. If there are any problems or issues, feel free to let me know! And if you'd like to see the code, visit my github page here: https://github.com/zicameau/dailyProgrammerPostingAndFormatting"
+
+disclaimer = disclaimer.replace(" ", " ^")
+
+text = text + "\n \n" + disclaimer
 
 print(text)
